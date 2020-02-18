e0f8c674e4e05b397ce28c3ab03907ed07b611d9
twead <45719524+twead@users.noreply.github.com>
2020-02-18 23:51:04 +0100

    SpringSecurity
    bejelentkeztetés egyéni adatokkal

--- src/main/java/com/sec/config/SecurityConf.java
+++ src/main/java/com/sec/config/SecurityConf.java
@@ -2,16 +2,17 @@ package com.sec.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 @EnableGlobalMethodSecurity(securedEnabled = true)
 @Configuration
 public class SecurityConf extends WebSecurityConfigurerAdapter {
 	
+	/*
 	@Autowired
 	public void configureAuth(AuthenticationManagerBuilder auth) throws Exception{
 		auth
@@ -24,6 +25,16 @@ public class SecurityConf extends WebSecurityConfigurerAdapter {
              .password("pass")
              .roles("ADMIN");
 	}
+	*/
+	
+	@Autowired
+	private UserDetailsService userService;
+	
+	@Autowired
+	public void configureAuth(AuthenticationManagerBuilder auth) throws Exception{
+		auth.userDetailsService(userService);
+	}
+
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
