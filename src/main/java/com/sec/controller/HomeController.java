dcaff683b6c6c9b32dfef067f453737ddbe98fe1
twead <45719524+twead@users.noreply.github.com>
2020-02-21 19:54:29 +0100

    fix

--- src/main/java/com/sec/controller/HomeController.java
+++ src/main/java/com/sec/controller/HomeController.java
@@ -65,6 +65,6 @@ public class HomeController {
 	@RequestMapping(path = "/activation/{code}", method = RequestMethod.GET)
 	public String activation(@PathVariable("code") String code, HttpServletResponse response) {
 		userService.userActivation(code);
-		return "auth/login?activationsuccess";
+		return "auth/login";
 	}
 }
